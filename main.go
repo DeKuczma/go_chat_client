f10702132af57f67f67539d0f9fa0f547cfe0d74
DeKuczma <dawid.j.kuczma@gmail.com>
2025-10-11 20:51:44 +0200

    Added websocket connection to server

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/url"
 
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	url := url.URL{
+		Scheme: "ws",
+		Host:   "127.0.0.1:8080",
+		Path:   "/ws",
+	}
+	conn, _, err := websocket.DefaultDialer.Dial(url.String(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	log.Println("Connextion via websocket established")
 }
